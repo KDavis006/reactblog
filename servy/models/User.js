653c70c87bd4b9b45eed7725a41d17c92c182e23
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2023-12-14 15:18:54 -0700

    yay

--- servy/models/User.js
+++ servy/models/User.js
@@ -12,6 +12,11 @@ const personSchema = new mongoose.Schema(
       trim: true,
       maxLength: [20, 'The name can not exceed 20 chars'],
     },
+    image: {
+      type: String,
+      required: false,
+      default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+    },
     email: {
       type: String,
       required: true,
